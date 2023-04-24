d5deaa48c27594c9f97f2a8cd338666b01163203
migueltorresvalls <mtorresvalls1@gmail.com>
2023-04-24 16:23:39 +0200

    v1

---
+++ search.js
@@ -0,0 +1,51 @@
+async function getData(){
+    let repos = [];
+    const username = document.getElementById("search-bar").value;
+    const url = `https://api.github.com/users/${username}/repos`;
+
+    const response = await fetch(url);
+
+    if (response.ok){
+        const data = await response.json();
+
+        // Se añade un elemento al final del array con .push
+        data.forEach(repo => {
+            let name = repo.name; 
+            let full_name = repo.full_name; 
+            let avatar_url = repo.owner.avatar_url;
+            let html_url = repo.html_url; 
+
+            let obj = {
+                name : name,
+                full_name : full_name, 
+                avatar_url : avatar_url,
+                html_url : html_url
+            };
+            
+            repos.push(obj);
+        })
+        return repos; 
+    } else {
+        throw new Error("Error en la busqueda de repositorios");
+    }
+}
+
+async function showData(){
+    try {
+        let repos = await getData();
+
+        let max = 20;
+        let index = 0;
+        var html = ``;
+        while (index < max){
+            if (repos.length > index){
+                html += `<div class=search-result><div class="left-content"><img src="${repos[index].avatar_url}"></div><div class="right-content"><p id="full-name">${repos[index].full_name}</p><a href="${repos[index].html_url}" id="name">${repos[index].name}</a></div></div>`;
+
+            }
+            index ++;
+        }
+        document.getElementById("search-result-container").innerHTML = html;
+    }catch (error){
+        alert(error);
+    }
+}
